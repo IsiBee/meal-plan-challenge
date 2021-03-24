3e0ff13af30e9a1aa10583292071d2051e66cdc1
albenchris <albenchris00@gmail.com>
2021-03-24 09:51:19 -0500

    route to get single recipe

--- controllers/api/recipe-routes.js
+++ controllers/api/recipe-routes.js
@@ -43,4 +43,58 @@ router.get("/", (req, res) => {
         .catch(err => res.status(500).json(err));
 });
 
+// GET single recipe ".../api/recipes/:id"
+router.get("/:id", (req, res) => {
+    Recipe.findOne({
+        where: { id: req.params.id },
+        attributes: [
+            "id",
+            "recipe_name",
+            "description",
+            "created_at",
+            "prep_time",
+            "cook_time",
+            "cooking_instructions",
+            "is_spicy",
+            "ingredient_id",
+            "weekday",
+            "user_id"
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: [
+                    "id",
+                    "comment_text",
+                    "recipe_id",
+                    "user_id",
+                    "created_at"
+                ],
+                include: {
+                    model: User,
+                    attributes: ["username"]
+                }
+            },
+            {
+                model: User,
+                attributes: ["username"]
+            }
+        ]
+    })
+        .then(dbRecipeData => {
+            if (!dbRecipeData) return res.status(404).json({ message: "No recipe found with this id"});
+
+            res.json(dbRecipeData);
+        })
+        .catch(err => res.status(500).json(err));
+});
+
+// POST create new recipe ".../api/recipes"
+
+// UPVOTE route
+
+// PUT update recipe ".../api/recipes/:id"
+
+// DELETE a recipe ".../api/recipes/:id"
+
 module.exports = router;
