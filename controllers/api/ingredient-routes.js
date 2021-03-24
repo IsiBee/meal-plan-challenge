15d96d6edfedeceab8808af0952edd14139d0e42
albenchris <albenchris00@gmail.com>
2021-03-24 10:48:32 -0500

    initial route to get single ingredient, REVISIT when closer to MVP

--- controllers/api/ingredient-routes.js
+++ controllers/api/ingredient-routes.js
@@ -40,6 +40,47 @@ router.get('/', (req, res) => {
 // ^^^ REQUIRES ATTENTION ^^^^^^
 
 // GET single ingredient ".../api/ingredients/:id"
+router.get("/:id", (req, res) => {
+    Ingredient.findOne({
+        where: { id: req.params.id },
+        attributes: [
+            "id",
+            "ingredient_name",
+            "is_gluten_free",
+            "is_vegetarian",
+            "is_vegan",
+            "is_keto"
+        ],
+        include: [
+            // maybe this will allow searching by ingredients?
+            {
+                model: Recipe,
+                attributes: [
+                    "id",
+                    "recipe_name",
+                    "servings",
+                    "is_spicy",
+                    "user_id"
+                ],
+                include: {
+                    model: User,
+                    attributes: ["username"]
+                }
+            },
+            {
+                model: User,
+                attributes: ["username"]
+            }
+        ]
+    })
+        .then(dbIngredientData => {
+            if(!dbIngredientData) return res.status(404).json({ message: "No ingredient found with this id" });
+
+            res.json(dbIngredientData);
+        })
+        .catch(err => res.status(500).json(err));
+});
+// ^^^ REQUIRES ATTENTION ^^^^^^
 
 // POST create ingredient ".../api/ingredients"
 
