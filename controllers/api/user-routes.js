7a78e154014173b6b3f70e1ca711bf8d8a75e025
albenchris <albenchris00@gmail.com>
2021-03-24 09:13:36 -0500

    route to get user by id

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Recipe, Comment } = require('../../models');
 
 // GET all users ".../api/users"
 router.get("/", (req, res) => {
@@ -7,6 +7,50 @@ router.get("/", (req, res) => {
         attributes: { exclude: ["password"] }
     })
         .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.status(500).json(err));
+});
+
+// GET user by id ".../api/users/:id"
+router.get("/:id", (req, res) => {
+    User.findOne({
+        attributes: { exclude: ["password"] },
+        where: { id: req.params.id },
+        include: [
+            {
+                model: Recipe,
+                attributes: [
+                    "id",
+                    "recipe_name",
+                    "description",
+                    "created_at",
+                    "prep_time",
+                    "cook_time",
+                    "cooking_instructions",
+                    "is_spicy",
+                    "ingredient_id",
+                    "weekday",
+                    "user_id"
+                ]
+            },
+            {
+                model: Comment,
+                attributes: [
+                    "id",
+                    "comment_text",
+                    "created_at"
+                ],
+                include: {
+                    model: Recipe,
+                    attributes: ["recipe_name"]
+                }
+            }
+        ]
+    })
+        .then(dbUserData => {
+            if (!dbUserData) return res.status(404).json({ message: "No user found with this id" });
+
+            res.json(dbUserData);
+        })
         .catch(err => res.status(500).json(err));
 });
 
