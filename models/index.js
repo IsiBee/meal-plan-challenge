bc41ec60115f849a36dbd4d1b87f5e363ee4739b
Sophia Mazurek <sophiamazurek@Sophias-MBP-2.lan>
2021-03-25 20:21:15 -0500

    added index.js file

---
+++ models/index.js
@@ -0,0 +1,38 @@
+const User = require("./User");
+
+const Ingredient = require("./Ingredient");
+
+const Comment = require("./comment");
+const Recipe = require("./Recipe");
+
+// ==== User/Recipe ===========
+User.hasMany(Recipe, {
+    foreignKey: "user_id"
+});
+
+Recipe.belongsTo(User, {
+    foreignKey: "user_id",
+    onDelete: "SET NULL"
+});
+// ==== User/Comment ========
+
+User.hasMany(Comment, {
+    foreignKey: "user_id",
+    onDelete: "SET NULL"
+});
+
+Comment.belongsTo(User, {
+    foreignKey: "user_id",
+    onDelete: "SET NULL"
+});
+// ==== Ingredient/Recipe ========
+
+Recipe.hasMany(Ingredient, {
+});
+
+Ingredient.belongsTo(Recipe, {
+    foreignKey: "post_id",
+    onDelete: "SET NULL"
+});
+
+module.exports = { User, Ingredient, Comment, Recipe };
