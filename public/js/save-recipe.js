9b4d04d1a7b6b348ee21b21da94ba896a3031937
albenchris <albenchris00@gmail.com>
2021-03-29 19:58:25 -0500

    preparing handlebars files

--- public/js/save-recipe.js
+++ public/js/save-recipe.js
@@ -1,7 +1,13 @@
 async function addToPlannerHandler(event) {
     event.preventDefault();
 
-    console.log("adding to planner!")
+    const id = window.location.toString().split("/")[
+        window.location.toString().split("/").length - 1
+    ];
+
+
+
+    console.log(id);
 };
 
 document.getElementById('add-to-planner').addEventListener('click', addToPlannerHandler);
