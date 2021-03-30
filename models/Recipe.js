6faaff98c3549105373e21b21c34ef7f569ffb92
albenchris <albenchris00@gmail.com>
2021-03-29 19:51:23 -0500

    added Meal Plan route to save recipe to user's meal plan

--- models/Recipe.js
+++ models/Recipe.js
@@ -4,7 +4,44 @@ const {Model, DataTypes } = require ('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Recipe extends Model {}
+class Recipe extends Model {
+    static saveRecipe(body, models) {
+        return models.MealPlan.create({
+            user_id: body.user_id,
+            recipe_id: body.recipe_id
+        })
+            .then(() => {
+                return Recipe.findOne({
+                    where: { id: body.recipe_id },
+                    attributes: [
+                        "id",
+                        "recipe_name",
+                        "description",
+                        "servings",
+                        "prep_time",
+                        "cook_time",
+                        "cooking_instructions",
+                        "is_spicy",
+                        "user_id"
+                    ],
+                    include: [
+                        {
+                            model: models.Ingredient,
+                            attributes: ["id", "ingredient_name", "preparation", "recipe_id"]
+                        },
+                        {
+                            model: models.Comment,
+                            attributes: ["id", "comment_text", "recipe_id", "user_id", "created_at"],
+                            include: {
+                                model: models.User,
+                                attributes: ["username"]
+                            }
+                        }
+                    ]
+                });
+            });
+    }
+}
 
 Recipe.init(
     {
